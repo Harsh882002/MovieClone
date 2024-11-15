60ceb5ed0363cd64cc51662fe440549c43317edd
Harsh Patle <harshpatle491@gmail.com>
2024-11-15 19:30:30 +0530

    new

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,10 @@
+export const Footer = () =>{
+
+    return(
+
+        <>
+        
+        <h1>I am from Footer</h1>
+        </>
+    )
+}
---
+++ src/header/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+
+    return (
+        <>
+           <h1>I am from Header</h1>
+        </>
+
+    )
+}

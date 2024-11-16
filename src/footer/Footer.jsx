9f6822d674527252934f88b47461ad7f2c7fd992
Harsh Patle <harshpatle491@gmail.com>
2024-11-16 13:33:33 +0530

    new

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -3,8 +3,10 @@ export const Footer = () =>{
     return(
 
         <>
+        <div className="fixed  flex items-center justify-center bottom-0 w-full h-1/4 bg-black text-white  ">
+        <h1 className="">I am from Footer</h1>
+        </div>
         
-        <h1>I am from Footer</h1>
         </>
     )
 }
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,7 +2,18 @@ export const Header = () => {
 
     return (
         <>
-           <h1>I am from Header</h1>
+        
+
+        <div className="relative bg-blue-400 h-6 w-full top-0.5 rounded ">
+        <h2 className="absolute left-4 font-bold text-red-700">DhekoFlix</h2>
+
+            <ul className=" absolute top-0 right-16 text-sm flex space-x-3 p-0 font-bold  ">
+                <li>Home</li>  
+                <li>About</li>
+                <li>Movies</li>
+                <li>Contact</li>
+            </ul>
+            </div>
         </>
 
     )

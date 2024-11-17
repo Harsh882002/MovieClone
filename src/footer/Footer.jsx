c16394229eab17c3ee51adaef7abd1d3dc43b881
Harsh Patle <harshpatle491@gmail.com>
2024-11-17 18:02:55 +0530

    harsh

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,22 +1,32 @@
-export const Footer = () =>{
+export const Footer = () => {
 
-    return(
+    return (
         <>
-       <div>
-        <ul>
-            <li>FAQ</li>
-            <li>Investor Relations</li>
-            <li>Privacy</li>
-            <li>Speed Test</li>
-            <li>Help Center</li>
-            <li>Jobs</li>
-            <li>Accounts</li>
-            <li>ways to watch</li>
-            <li>Term and Use</li>
-            <li>Contact us</li>
-        </ul>
-       </div>
-        
+            <div className="fixed bottom-3 bg-black w-full text-white flex items-center justify-center ">
+                <ul className="ml-15 mr-14">
+                    
+                    <li>FAQ</li>
+                    <li>Investor Relations</li>
+                    <li>Privacy</li>
+                   
+
+                </ul>
+
+                <ul className="ml-14 mr-14">
+                <li>Speed Test</li>
+                <li>Help Center</li>
+                <li>Jobs</li>
+                </ul>
+
+                <ul className="ml-14 mr-14">
+                    
+                    <li>Accounts</li>
+                    <li>ways to watch</li>
+                    <li>Term and Use</li>
+                    <li>Contact us</li>
+                </ul>
+            </div>
+
         </>
     )
 }

aa5afcf3f753b1a31495e78f1e0cfea730a5e203
Harsh Patle <harshpatle491@gmail.com>
2024-11-17 17:40:27 +0530

    new

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,10 +1,9 @@
 export const Footer = () =>{
 
     return(
-
         <>
         <div className="fixed  flex items-center justify-center bottom-0 w-full h-1/4 bg-black text-white  ">
-        <h1 className="">I am from Footer</h1>
+        <h1 className="">I am from Footer and you </h1>
         </div>
         
         </>
